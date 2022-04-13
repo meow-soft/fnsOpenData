ff9a576abc3cfca9f1baf94dfea5c7db55a36535
ybogdanov <79097146710@yandex.ru>
2022-04-13 13:53:23 +0400

    graphql

---
+++ src/main/java/meow/soft/fnsopendata/service/QueryService.java
@@ -0,0 +1,24 @@
+package meow.soft.fnsopendata.service;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import lombok.RequiredArgsConstructor;
+import meow.soft.fnsopendata.model.TaxAuthority;
+import meow.soft.fnsopendata.repo.TaxAuthorityRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QueryService implements GraphQLQueryResolver {
+
+    private final TaxAuthorityRepository repository;
+
+    public List<TaxAuthority> list() {
+        return repository.findAll();
+    }
+
+    public TaxAuthority tnoByCode(String code) {
+        return repository.findByCode(code);
+    }
+}
